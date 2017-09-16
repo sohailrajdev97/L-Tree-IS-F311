4bcef27178013aa16008b5b44cc89f7c25cbe898
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-09-16 15:27:21 +0530

    glut: Add full screen support

--- L-Tree/L-Tree/main.cpp
+++ L-Tree/L-Tree/main.cpp
@@ -7,20 +7,41 @@
 #include <iostream>
 #include "Lindenmayer.h"
 
+void init();
+void draw();
+void processKeys(unsigned char, int, int);
+
+int main(int argc, char **argv)
+{
+    
+    glutInit(&argc, argv);
+    init();
+    glutMainLoop();
+    return 0;
+    
+}
+
 void init() {
+    
     int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
     int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
-    
+
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(screenWidth, screenHeight);
-    glutCreateWindow("Lines");
+    glutCreateWindow("L-Systems");
+    
+    glutKeyboardFunc(processKeys);
+    glutDisplayFunc(draw);
+    
     glClearColor(0.0, 0.0, 0.0, 0);
     gluOrtho2D(0, screenWidth, 0, screenHeight);
+    glutFullScreen();
     
 }
 
 void draw() {
+    
     glClear(GL_COLOR_BUFFER_BIT);
     double colors[][3] = {{1.0, 1.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0}, {0.0, 1.0, 1.0}};
     Lindenmayer tree1;
@@ -32,15 +53,17 @@ void draw() {
     tree1.addRule("X=C0FF+[C1+F]+[C3-F]");
     std::cout<<tree1.getAxiom()<<endl;
     tree1.draw(4, colors);
-    
     glFlush();
+    
 }
 
-int main(int argc, char **argv)
-{
-    glutInit(&argc, argv);
-    init();
-    glutDisplayFunc(draw);
-    glutMainLoop();
-    return 0;
+void processKeys(unsigned char key, int x, int y){
+    
+    switch(key){
+        case 27:
+            exit(0);
+            break;
+    }
+    
+    glutPostRedisplay();
 }
